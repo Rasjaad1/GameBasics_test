839107e78c8f53013e528589b79c35c44db7c9a4
Rasjaad <rasjaad@live.nl>
2021-09-13 13:43:59 +0200

    First commit

---
+++ app/src/main/java/com/example/footballsimulation/Team.java
@@ -0,0 +1,77 @@
+package com.example.footballsimulation;
+
+public class Team {
+
+    private final String teamName;
+    private final int teamAttackingStrength;
+    private final int teamDefensiveStrength;
+    private int score;
+    private Integer points;
+    private int win;
+    private int tie;
+    private int loss;
+
+    public Team(String teamName, int teamAttackingStrength, int teamDefensiveStrength) {
+        this.teamName = teamName;
+        this.teamAttackingStrength = teamAttackingStrength;
+        this.teamDefensiveStrength = teamDefensiveStrength;
+        score = 0;
+        points = 0;
+        win = 0;
+        tie = 0;
+        loss = 0;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public int getteamAttackingStrength() {
+        return teamAttackingStrength;
+    }
+
+    public int getTeamDefensiveStrength() {
+        return teamDefensiveStrength;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setTie(int tie) {
+        this.tie = tie;
+    }
+
+    public int getTie() {
+        return tie;
+    }
+
+    public void setLoss(int loss) {
+        this.loss = loss;
+    }
+
+    public int getLoss() {
+        return loss;
+    }
+}
+
