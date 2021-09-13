29147a1c97b0107e80af6c9f98dab325901515b2
Rasjaad <rasjaad@live.nl>
2021-09-13 17:53:02 +0200

    Added PointSorter class to sort arraylist of teams

--- app/src/main/java/com/example/footballsimulation/PointSorter.java
+++ app/src/main/java/com/example/footballsimulation/PointSorter.java
@@ -1,4 +1,11 @@
 package com.example.footballsimulation;
 
-public class PointSorter {
+import java.util.Comparator;
+
+public class PointSorter implements Comparator<Team>
+{
+    @Override
+    public int compare(Team t1, Team t2) {
+        return t2.getPoints().compareTo(t1.getPoints());
+    }
 }
