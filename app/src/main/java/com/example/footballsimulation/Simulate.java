7be2ddd6549b2e42b55113dc9562837e95810687
Rasjaad <rasjaad@live.nl>
2021-09-13 17:53:56 +0200

    added Simulate class that connects to activities

--- app/src/main/java/com/example/footballsimulation/Simulate.java
+++ app/src/main/java/com/example/footballsimulation/Simulate.java
@@ -1,4 +1,15 @@
 package com.example.footballsimulation;
 
 public class Simulate {
+
+    Helper helper = new Helper();
+    public void simulateRound(Team t1, Team t2, Team t3, Team t4) {
+        Match match = new Match();
+
+        match.MatchResult(t1, t2);
+        match.MatchResult(t2, t1);
+        match.MatchResult(t3, t4);
+        match.MatchResult(t4, t3);
+        helper.sortTeamsByPoints(t1, t2, t3, t4);
+    }
 }
