1c04a5f9ea770ca9e28c034f83306a31e5577633
Rasjaad <rasjaad@live.nl>
2021-09-15 17:43:19 +0200

    Transfered sorting method to EndREsultActivity

--- app/src/main/java/com/example/footballsimulation/Helper.java
+++ app/src/main/java/com/example/footballsimulation/Helper.java
@@ -27,28 +27,12 @@ public class Helper {
         return Math.abs(odds);
     }
 
-
-    public void sortTeamsByPoints(Team t1, Team t2, Team t3, Team t4) {
-        ArrayList<Team> items = new ArrayList<>();
-        items.add(t1);
-        items.add(t2);
-        items.add(t3);
-        items.add(t4);
-
-        items.sort(new PointSorter());
-        System.out.println("Club:             Points:                   Goals:                  Conceded:                 Goal Difference:  ");
-        for (int i = 0; i < items.size(); i++) {
-            System.out.println(items.get(i).getTeamName() + "               " + items.get(i).getPoints() + "            " + items.get(i).getScore() + "           " + Math.abs(items.get(i).getConceded()) + "            " + items.get(i).getGoalDifference());
-        }
-    }
-
-
     public void countDownTime(Team t1, Team t2) {
         int time = 0;
         do {
             time++;
 
-            if (hasScored(t1, t2)) ;
+            if (hasScored(t1, t2)) break;
             else hasScored(t2, t1);
 
         } while (time != 90);
@@ -62,7 +46,6 @@ public class Helper {
         if (chance <= odds) {
             goal = (int) ((Math.random() * 1) * (t1.getteamAttackingStrength() - t2.getTeamDefensiveStrength()));
             if (goal == 1) {
-                //System.out.println("true");
                 changeScoreAndConceded(t1, t2);
                 return true;
             }
