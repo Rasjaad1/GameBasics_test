7aaaf97607e48df5c5f40efda494657b82147515
Rasjaad <rasjaad@live.nl>
2021-09-15 17:45:19 +0200

    Removed sorting method from Helper class

--- app/src/main/java/com/example/footballsimulation/Simulate.java
+++ app/src/main/java/com/example/footballsimulation/Simulate.java
@@ -2,7 +2,6 @@ package com.example.footballsimulation;
 
 public class Simulate {
 
-    Helper helper = new Helper();
     public void simulateRound(Team t1, Team t2, Team t3, Team t4) {
         Match match = new Match();
 
@@ -10,6 +9,5 @@ public class Simulate {
         match.MatchResult(t2, t1);
         match.MatchResult(t3, t4);
         match.MatchResult(t4, t3);
-        helper.sortTeamsByPoints(t1, t2, t3, t4);
     }
 }
