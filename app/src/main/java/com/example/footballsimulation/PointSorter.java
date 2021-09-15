6fb3a9f94b71e9fcf284ccbd24949f0ba5ca6fab
Rasjaad <rasjaad@live.nl>
2021-09-15 17:44:49 +0200

    Added a second sorting comparator to check for GD

--- app/src/main/java/com/example/footballsimulation/PointSorter.java
+++ app/src/main/java/com/example/footballsimulation/PointSorter.java
@@ -2,10 +2,19 @@ package com.example.footballsimulation;
 
 import java.util.Comparator;
 
-public class PointSorter implements Comparator<Team>
-{
+public class PointSorter implements Comparator<Team> {
+
     @Override
     public int compare(Team t1, Team t2) {
-        return t2.getPoints().compareTo(t1.getPoints());
+
+
+        int sComp = t2.getPoints().compareTo(t1.getPoints());
+
+        if (sComp != 0) {
+            return sComp;
+        }
+
+
+        return t2.getGoalDifference().compareTo(t1.getGoalDifference());
     }
 }
