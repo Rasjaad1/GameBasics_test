871faf691e5460a490b122469ebfa244f894dbbd
Rasjaad <rasjaad@live.nl>
2021-09-16 20:58:23 +0200

    Added way to show all individual matches and results

--- app/src/main/java/com/example/footballsimulation/EndResultActivity.java
+++ app/src/main/java/com/example/footballsimulation/EndResultActivity.java
@@ -12,19 +12,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-import static com.example.footballsimulation.Match.matches;
-
 public class EndResultActivity extends AppCompatActivity {
-    public ArrayList<Team> items = new ArrayList<>();
+    public ArrayList<Team> leagueTableList = new ArrayList<>();
+    public ArrayList<Team> individualMatchListHome = new ArrayList<>();
+    public ArrayList<Team> individualMatchListAway = new ArrayList<>();
 
+
+    TextView teamHome;
     Team ajax = new Team("Ajax", 85, 78);
-    Team psv = new Team("psv", 83, 79);
+    Team psv = new Team("psv", 82, 77);
     Team ado = new Team("ado", 72, 65);
     Team feyenoord = new Team("feyenoord", 76, 73);
-    Simulate simulate = new Simulate();
 
-    TableLayout tablelayout;
-    TextView txt;
+    Match match = new Match();
+    Helper helper = new Helper();
+
+    TableLayout league_table_layout, individual_matches_tablelayout;
+    int matchNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +38,56 @@ public class EndResultActivity extends AppCompatActivity {
 
         Switch layoutSwitch = findViewById(R.id.switch1);
 
-        simulate.simulateRound(ajax, psv, ado, feyenoord);
-        simulate.simulateRound(ajax, ado, psv, feyenoord);
-        simulate.simulateRound(ajax, feyenoord, ado, psv);
-        sortTeams();
+        league_table_layout = findViewById(R.id.league_table_layout);
+        individual_matches_tablelayout = findViewById(R.id.individual_matches_tablelayout);
+        individual_matches_tablelayout.setVisibility(View.INVISIBLE);
+        teamHome = findViewById(R.id.teamHome);
+        teamHome.setVisibility(View.VISIBLE);
 
-        tablelayout = (TableLayout) findViewById(R.id.tablelayout);
-        txt = findViewById(R.id.testtxt);
-        txt.setVisibility(View.INVISIBLE);
 
+        //Round 1
+        match.playMatch(ajax, psv, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(psv, ajax, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(ado, feyenoord, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(feyenoord, ado, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+
+        //Round 2
+        match.playMatch(ajax, feyenoord, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(feyenoord, ajax, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(psv, ado, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(ado, psv, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        //Round 3
+        match.playMatch(ajax, ado, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(ado, ajax, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(psv, feyenoord, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+        match.playMatch(feyenoord, psv, individualMatchListHome, individualMatchListAway);
+        fillMatchup();
+
+        helper.sortTeams(leagueTableList, ajax, psv, feyenoord, ado);
         fillLeagueTable();
-        fillLeagueMatchups();
 
         layoutSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    tablelayout.setVisibility(View.INVISIBLE);
-                    txt.setVisibility(View.VISIBLE);
-
+                    league_table_layout.setVisibility(View.INVISIBLE);
+                    teamHome.setVisibility(View.INVISIBLE);
+                    individual_matches_tablelayout.setVisibility(View.VISIBLE);
                 } else {
-                    tablelayout.setVisibility(View.VISIBLE);
-                    txt.setVisibility(View.INVISIBLE);
+                    league_table_layout.setVisibility(View.VISIBLE);
+                    teamHome.setVisibility(View.VISIBLE);
+                    individual_matches_tablelayout.setVisibility(View.INVISIBLE);
                 }
             }
 
@@ -63,20 +95,10 @@ public class EndResultActivity extends AppCompatActivity {
 
     }
 
-    public void sortTeams() {
-        //clears previous objects, updates values and sorts them through points and goal difference.
-        items.clear();
-        items.add(ajax);
-        items.add(psv);
-        items.add(ado);
-        items.add(feyenoord);
-        items.sort(new PointSorter());
-    }
-
     public void fillLeagueTable() {
         //Fills tablerow with values of items arraylist
-        for (Team ar : items) {
-            final TableRow tableRow = (TableRow) getLayoutInflater().inflate(R.layout.tablerow, null);
+        for (Team ar : leagueTableList) {
+            final TableRow tableRow = (TableRow) getLayoutInflater().inflate(R.layout.league_table_row, null);
             TextView tv;
 
             //Filling in cells
@@ -96,7 +118,7 @@ public class EndResultActivity extends AppCompatActivity {
             tv.setText(String.valueOf(ar.getLoss()));
 
             tv = (TextView) tableRow.findViewById(R.id.tableCell6);
-            tv.setText(String.valueOf(ar.getScore()));
+            tv.setText(String.valueOf(ar.getTotalScore()));
 
             tv = (TextView) tableRow.findViewById(R.id.tableCell7);
             tv.setText(String.valueOf(ar.getConceded()));
@@ -105,14 +127,46 @@ public class EndResultActivity extends AppCompatActivity {
             tv.setText(String.valueOf(ar.getGoalDifference()));
 
             //Add row to the table
-            tablelayout.addView(tableRow);
+            league_table_layout.addView(tableRow);
         }
     }
 
-    public void fillLeagueMatchups() {
+    public void fillMatchup() {
+        for (Team ar : individualMatchListHome) {
+            for (Team ar2 : individualMatchListAway) {
 
+                final TableRow tableRow = (TableRow) getLayoutInflater().inflate(R.layout.individual_matches_table, null);
+                TextView tv;
+
+                //Filling in cells
+                tv = (TextView) tableRow.findViewById(R.id.tableCell0);
+                tv.setText(String.valueOf("Match: " + matchNumber));
+
+                tv = (TextView) tableRow.findViewById(R.id.tableCell1);
+                tv.setText(ar.getTeamName());
+
+                tv = (TextView) tableRow.findViewById(R.id.tableCell2);
+                tv.setText(String.valueOf(ar.getScore()));
+
+                tv = (TextView) tableRow.findViewById(R.id.tableCell3);
+                tv.setText(R.string.vs);
+
+                tv = (TextView) tableRow.findViewById(R.id.tableCell4);
+                tv.setText(String.valueOf(ar2.getTeamName()));
+
+                tv = (TextView) tableRow.findViewById(R.id.tableCell5);
+                tv.setText(String.valueOf(ar2.getScore()));
+
+                //Add row to the table
+                individual_matches_tablelayout.addView(tableRow);
+                ++matchNumber;
+                ar2.clearScore();
+            }
+            ar.clearScore();
+        }
+        individualMatchListHome.clear();
+        individualMatchListAway.clear();
         //ToDo need to add a way to seperate and count score of every match.
-        txt.setText(String.valueOf(matches.get(0).getScore()));
     }
 
 }
