3e2577793f004d870b56a0bc482cea28a8ab9c19
Rasjaad <rasjaad@live.nl>
2021-09-16 23:48:31 +0200

    Changed name of the 2 methods

--- app/src/main/java/com/example/footballsimulation/Helper.java
+++ app/src/main/java/com/example/footballsimulation/Helper.java
@@ -5,16 +5,16 @@ import java.util.*;
 public class Helper {
 
 
-    public void changeScoreAndConceded(Team teamHome, Team teamAway) {
+    public void changeScoreAndConcededPoints(Team teamHome, Team teamAway) {
         teamHome.setScore(teamHome.getScore() + 1);
         teamHome.setTotalScore(teamHome.getTotalScore() + 1);
         teamAway.setConceded(teamAway.getConceded() - 1);
 
-        teamHome.setGoalDifference(teamHome.getTotalScore() - setNegativeNumberToPositive(teamHome));
-        teamAway.setGoalDifference(teamAway.getTotalScore() - setNegativeNumberToPositive(teamAway));
+        teamHome.setGoalDifference(teamHome.getTotalScore() - changeNegativeNumberToPositive(teamHome));
+        teamAway.setGoalDifference(teamAway.getTotalScore() - changeNegativeNumberToPositive(teamAway));
     }
 
-    public int setNegativeNumberToPositive(Team team) {
+    public int changeNegativeNumberToPositive(Team team) {
         return Math.abs(team.getConceded());
     }
 
@@ -26,32 +26,6 @@ public class Helper {
         odds = teamHome.getteamAttackingStrength() - teamAway.getTeamDefensiveStrength();
         odds = odds / 10;
         return Math.abs(odds);
-    }
-
-    public void countDownTime(Team teamHome, Team teamAway) {
-        for (int i = 0; i <= 90; i++) {
-            if (hasScored(teamHome, teamAway) == 1) {
-                changeScoreAndConceded(teamHome, teamAway);
-                ++i;
-            } else if (hasScored(teamAway, teamHome) == 1) {
-                changeScoreAndConceded(teamAway, teamHome);
-                ++i;
-            }
-        }
-    }
-
-    public int hasScored(Team teamHome, Team teamAway) {
-        int goal = 0;
-        Random r = new Random();
-        float odds = changeOdds(teamHome, teamAway);
-        float chance = r.nextFloat();
-        if (chance <= odds) {
-            goal = (int) ((Math.random() * 1) * (teamHome.getteamAttackingStrength() - teamAway.getTeamDefensiveStrength()));
-            if (goal == 1) {
-                return goal;
-            }
-        }
-        return goal;
     }
 
     public void sortTeams(ArrayList<Team> leagueTableList, Team team1, Team team2, Team team3, Team team4) {
